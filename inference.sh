26c18bdccb8f397f459a1b71b87fdba308840a11
HieuNV1Apero <HieuNV1@apero.vn>
2025-03-12 11:15:32 +0700

    Remove pip requirements from conda env, add requirements.txt

--- inference.sh
+++ inference.sh
@@ -2,7 +2,7 @@
 ##paired setting
 accelerate launch inference.py --pretrained_model_name_or_path "yisol/IDM-VTON" \
     --width 768 --height 1024 --num_inference_steps 30 \
-    --output_dir "result" --data_dir "/home/omnious/workspace/yisol/Dataset/zalando" \
+    --output_dir "result" --data_dir "data" \
     --seed 42 --test_batch_size 2 --guidance_scale 2.0
 
 
